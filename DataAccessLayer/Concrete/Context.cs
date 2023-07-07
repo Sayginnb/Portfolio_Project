92186e527ca8ca40db5fef7e4bbdb6359155054d
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-07-07 17:55:32 +0300

    Migration

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -12,7 +12,7 @@ namespace DataAccessLayer.Concrete
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server=DESKTOP - 917NIFO\\SQLEXPRESS;database=PortfolioProjectDb;integrated security=true");
+            optionsBuilder.UseSqlServer("server=DESKTOP-917NIFO\\SQLEXPRESS;database=PortfolioProjectDb;integrated security=true");
 
         }
         public DbSet<About> Abouts { get; set; }
