d765161cb1ab67de6dbaed4fb1dbaf5ae5f0824c
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-07-14 15:40:54 +0300

    Skill edit

--- BusinessLayer/Concrete/SkillManager.cs
+++ BusinessLayer/Concrete/SkillManager.cs
@@ -25,12 +25,12 @@ namespace BusinessLayer.Concrete
 
         public void TDelete(Skill t)
         {
-            throw new NotImplementedException();
+            _skillDal.Delete(t);
         }
 
         public Skill TGetById(int id)
         {
-            throw new NotImplementedException();
+            return _skillDal.GetById(id);
         }
 
         public List<Skill> TGetList()
@@ -40,7 +40,7 @@ namespace BusinessLayer.Concrete
 
         public void TUptade(Skill t)
         {
-            throw new NotImplementedException();
+            _skillDal.Update(t);
         }
     }
 }
--- Portfolio_Project/Controllers/SkillController.cs
+++ Portfolio_Project/Controllers/SkillController.cs
@@ -31,5 +31,27 @@ namespace Portfolio_Project.Controllers
             skillManager.TAdd(skill);
             return RedirectToAction("Index");
         }
+        public IActionResult DeleteSkill(int id)
+        {
+            var values = skillManager.TGetById(id);
+            skillManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditSkill(int id)
+        {
+            ViewBag.v1 = "Düzenleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Güncelleme";
+            var values = skillManager.TGetById(id);
+            return View(values);
+
+        }
+        [HttpPost]
+        public IActionResult EditSkill(Skill skill)
+        {
+            skillManager.TUptade(skill);
+            return RedirectToAction("Index");
+        }
     }
 }
