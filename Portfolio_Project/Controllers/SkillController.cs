d0929563a3d09872f69d9f463a185a1a951fe531
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-07-14 12:39:49 +0300

    Admin Layout editing

--- Portfolio_Project/Controllers/SkillController.cs
+++ Portfolio_Project/Controllers/SkillController.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Portfolio_Project.Controllers
 {
     public class SkillController : Controller
     {
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
+
         public IActionResult Index()
         {
-            return View();
+            var values = skillManager.TGetList();
+            return View(values);
         }
     }
 }
