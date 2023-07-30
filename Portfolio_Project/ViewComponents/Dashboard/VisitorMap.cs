7b155bd7b3a1d104ade027692bea90276a7c966c
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-07-30 17:42:59 +0300

    Visitor map editing

---
+++ Portfolio_Project/ViewComponents/Dashboard/VisitorMap.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Project.ViewComponents.Dashboard
+{
+    public class VisitorMap : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
