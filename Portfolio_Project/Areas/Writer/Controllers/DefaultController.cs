90629270776e13d5b21b76b25673a2ae090f358c
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-07-31 12:59:12 +0300

    Area Configration

---
+++ Portfolio_Project/Areas/Writer/Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Project.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class DefaultController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
