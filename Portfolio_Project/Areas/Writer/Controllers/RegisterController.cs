8acb9f1ea04bdf33a530498964577f99e7e9795d
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-08-01 12:07:47 +0300

    Register form added

---
+++ Portfolio_Project/Areas/Writer/Controllers/RegisterController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Project.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string p)
+        {
+            return View();
+        }
+    }
+}
