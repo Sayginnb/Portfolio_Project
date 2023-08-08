05748b34badec3078f8d4325460588a7ed9ba70e
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-08-08 14:08:48 +0300

    Announcement created

--- Portfolio_Project/Areas/Writer/Controllers/DefaultController.cs
+++ Portfolio_Project/Areas/Writer/Controllers/DefaultController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Portfolio_Project.Areas.Writer.Controllers
@@ -6,10 +8,11 @@ namespace Portfolio_Project.Areas.Writer.Controllers
     [Area("Writer")]
     public class DefaultController : Controller
     {
-
+        AnnouncementManager announcementManager = new AnnouncementManager(new EfAnnouncementDal());
         public IActionResult Index()
         {
-            return View();
+            var values = announcementManager.TGetList();
+            return View(values);
         }
     }
 }
