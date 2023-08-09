6235430eb476ac3f4fc6d0b1edc8fb4576347d40
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-08-09 13:28:59 +0300

    Update user view model

--- Portfolio_Project/Areas/Writer/Controllers/ProfileController.cs
+++ Portfolio_Project/Areas/Writer/Controllers/ProfileController.cs
@@ -1,6 +1,9 @@
 ﻿using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Portfolio_Project.Areas.Writer.Models;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Portfolio_Project.Areas.Writer.Controllers
@@ -19,6 +22,29 @@ namespace Portfolio_Project.Areas.Writer.Controllers
         {
             var values = await _userManager.FindByNameAsync(User.Identity.Name);
             return View(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel p)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (p.Picture != null)
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(p.Picture.FileName);
+                var imagename = Guid.NewGuid() + extension;
+                var savelocation = resource + "/wwwroot/userimage/" + imagename;
+                var stream = new FileStream(savelocation, FileMode.Create);
+                await p.Picture.CopyToAsync(stream);
+                user.ImageUrl = imagename;
+            }
+            user.Name = p.Name;
+            user.SurName = p.Surname;
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+            return View();
         }
     }
 }
