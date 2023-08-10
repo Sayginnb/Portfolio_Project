06e4df2934342f227567d01b7ee5668aeaebd074
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-08-10 15:08:05 +0300

    Writer control panel update

--- Portfolio_Project/Areas/Writer/Controllers/DashboardWriterController.cs
+++ Portfolio_Project/Areas/Writer/Controllers/DashboardWriterController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Portfolio_Project.Areas.Writer.Controllers
 {
     [Area("Writer")]
     public class DashboardWriterController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<WriterUser> _userManager;
+
+        public DashboardWriterController(UserManager<WriterUser> userManager)
         {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v = values.Name + " " + values.SurName;
+
+            // statistics
+            Context c = new Context();
+            ViewBag.v1 = 0;
+            ViewBag.v2 = c.Announcements.Count();
+            ViewBag.v3 = 0;
+            ViewBag.v4 = c.Skills.Count();
+
             return View();
         }
     }
