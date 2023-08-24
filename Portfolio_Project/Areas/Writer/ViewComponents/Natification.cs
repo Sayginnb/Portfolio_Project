e641a27c68b84f9b4b34367c769270943cf962f9
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-08-24 12:19:04 +0300

    Listing of notifications

---
+++ Portfolio_Project/Areas/Writer/ViewComponents/Natification.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Portfolio_Project.Areas.Writer.ViewComponents
+{
+    public class Natification : ViewComponent
+    {
+        AnnouncementManager announcementManager = new AnnouncementManager(new EfAnnouncementDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = announcementManager.TGetList().Take(5).ToList();
+            return View(values);
+        }
+    }
+}
