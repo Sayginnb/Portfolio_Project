fed295fdba24def1bd187ced0235400868be5bd8
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-08-26 15:54:35 +0300

    Revised

--- Portfolio_Project/Areas/Writer/Controllers/MessageController.cs
+++ Portfolio_Project/Areas/Writer/Controllers/MessageController.cs
@@ -1,9 +1,14 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace Portfolio_Project.Areas.Writer.Controllers
 {
@@ -41,6 +46,26 @@ namespace Portfolio_Project.Areas.Writer.Controllers
         {
             WriterMessage writerMessage = writerMessageManager.TGetById(id);
             return View(writerMessage);
+        }
+        [HttpGet]
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(WriterMessage p)
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            string mail = values.Email;
+            string name = values.Name + " " + values.SurName;
+            p.Date = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            p.Sender = mail;
+            p.SenderName = name;
+            Context c = new Context();
+            var usernamesurname=c.Users.Where(x => x.Email == p.Receiver).Select(y => y.Name + " " + y.SurName).FirstOrDefault();
+            p.ReceiverName = usernamesurname;
+            writerMessageManager.TAdd(p);
+            return RedirectToAction("SenderMessage","Message");
         }
     }
 }
