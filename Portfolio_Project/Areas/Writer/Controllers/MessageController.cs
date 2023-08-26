803548483f1bcc50621e8d8f26d8c3934fcbd42d
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-08-26 10:13:40 +0300

    Sender/Receiver message list

--- Portfolio_Project/Areas/Writer/Controllers/MessageController.cs
+++ Portfolio_Project/Areas/Writer/Controllers/MessageController.cs
@@ -18,11 +18,18 @@ namespace Portfolio_Project.Areas.Writer.Controllers
             _userManager = userManager;
         }
 
-        public async Task<IActionResult> Index(string p)
+        public async Task<IActionResult> ReceiverMessage(string p)
         {
             var values = await _userManager.FindByNameAsync(User.Identity.Name);
             p = values.Email;
             var messageList = writerMessageManager.GetListReceiverMessage(p);
+            return View(messageList);
+        }
+        public async Task<IActionResult> SenderMessage(string p)
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            p = values.Email;
+            var messageList = writerMessageManager.GetListSenderMessage(p);
             return View(messageList);
         }
     }
