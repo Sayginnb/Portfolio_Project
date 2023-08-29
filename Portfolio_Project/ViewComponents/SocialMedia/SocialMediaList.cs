fc8400aab1f4453a38df394c379fd3773d094122
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-08-29 17:09:11 +0300

    Social media list created

--- Portfolio_Project/ViewComponents/SocialMedia/SocialMediaList.cs
+++ Portfolio_Project/ViewComponents/SocialMedia/SocialMediaList.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,11 @@ namespace Portfolio_Project.ViewComponents.SocialMedia
 {
     public class SocialMediaList : ViewComponent
     {
+        SocialMediaManager socialMedia = new SocialMediaManager(new EfSocialMediaDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = socialMedia.TGetList();
+            return View(values);
         }
     }
 }
