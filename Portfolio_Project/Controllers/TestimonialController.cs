1e3a53680dcd90550701860266aeacf9d02c021c
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-08-29 12:52:16 +0300

    Testimonial index added

---
+++ Portfolio_Project/Controllers/TestimonialController.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Project.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
+        public IActionResult Index()
+        {
+            var values = testimonialManager.TGetList();
+            return View(values);
+        }
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var values = testimonialManager.TGetById(id);
+            testimonialManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditTestimonial(int id)
+        {
+            var values = testimonialManager.TGetById(id);
+            return View(values);
+
+        }
+        [HttpPost]
+        public IActionResult EditTestimonial(Testimonial testimonial)
+        {
+            testimonialManager.TUptade(testimonial);
+            return RedirectToAction("Index");
+        }
+    }
+}
