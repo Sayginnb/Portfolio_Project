814061bb9c76830e9051123e1202f0ce5957c4c6
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-08-30 09:46:12 +0300

    Message list desing

---
+++ Portfolio_Project/ViewComponents/Dashboard/AdminNavbarMessageList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Project.ViewComponents.Dashboard
+{
+    public class AdminNavbarMessageList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
